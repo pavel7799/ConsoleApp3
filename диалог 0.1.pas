432b635e66a4b37612492e7c99d05816c30bede6
pavel7799 <81684192+pavel7799@users.noreply.github.com>
2021-05-26 10:50:36 +0300

    Add files via upload

---
+++ диалог 0.1.pas
@@ -0,0 +1,12 @@
+﻿var a:string;
+begin
+writeln('Как тебя зовут ?');
+readln(a);
+writeln('Привет ',a,' !');
+writeln('Сколько тебе лет? ');
+readln();
+writeln('Какой у тебя любимый предмет в школе?');
+readln();
+writeln('Какая у тебя любимая книга');
+readln();
+end.
